4b2813f4b50d2c624bed67af8d4b4dd9048653a6
katerinacowan <105337013+katerinacowan@users.noreply.github.com>
2022-05-10 22:23:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Main{
+	public static void main(String[]args){
+		int b = -12;
+		while (b < 0)
+		{
+			b = b+2; 
+			System.out.println(b + " ");
+        }
+	
+	}
+}
